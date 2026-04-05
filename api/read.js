49b130a9e66bc1f517b79492b646a9778cd2fc35
shair1alii <zawarahmed462@gmail.com>
2026-04-05 23:03:00 +0500

    Update read.js

--- api/read.js
+++ api/read.js
@@ -1,8 +1,14 @@
 export default async function handler(req, res) {
   const { id } = req.query;
 
-  const response = await fetch(`https://www.emailondeck.com/email.php?msg_id=${id}`);
+  const response = await fetch(`https://www.emailondeck.com/email_iframe.php?msg_id=${id}`, {
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      "Cookie": "PHPSESSID=kd4d697p6je6v00na812jc5as"
+    }
+  });
+
   const text = await response.text();
 
-  res.json({ data: text });
+  res.send(text);
 }
