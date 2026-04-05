a0ff378dfa97dec3817665a21d8f235043d0a3df
shair1alii <zawarahmed462@gmail.com>
2026-04-05 23:02:26 +0500

    Update inbox.js

--- api/inbox.js
+++ api/inbox.js
@@ -1,6 +1,12 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://www.emailondeck.com/ajax/messages.php");
+  const response = await fetch("https://www.emailondeck.com/ajax/messages.php", {
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      "Cookie": "PHPSESSID=kd4d697p6je6v00na812jc5as"
+    }
+  });
+
   const text = await response.text();
 
-  res.json({ data: text });
+  res.send(text);
 }
