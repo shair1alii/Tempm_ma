c6fbb3339d33f59c897286b1633457bc1dccbca8
shair1alii <zawarahmed462@gmail.com>
2026-04-06 10:50:57 +0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,18 +1,16 @@
 export default async function handler(req, res) {
   const response = await fetch("https://www.emailondeck.com/eod.php", {
-    method: "GET",
     headers: {
-      "User-Agent": "Mozilla/5.0",
-      "Cookie": "PHPSESSID=kd4d697p6je6v00na812jc5as"
+      "User-Agent": "Mozilla/5.0"
     }
   });
 
   const text = await response.text();
 
-  // Email extract (basic)
-  const match = text.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}/);
+  // better email match
+  const match = text.match(/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}/);
 
   res.json({
-    email: match ? match[0] : "not found"
+    email: match ? match[0] : "email not found"
   });
 }
