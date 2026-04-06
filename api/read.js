6e2db610220e6b6a7ef17ac59e6dc4b578a6d12d
shair1alii <zawarahmed462@gmail.com>
2026-04-06 11:27:17 +0500

    Update read.js

--- api/read.js
+++ api/read.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const response = await fetch(`https://www.emailondeck.com/email_iframe.php?msg_id=${id}`, {
     headers: {
       "User-Agent": "Mozilla/5.0",
-      "Cookie": "PHPSESSID=kd4d697p6je6v00na812jc5as"
+      "Cookie": "PHPSESSID=3isjbg118897gsdqaf4uk7f4pb"
     }
   });
 
